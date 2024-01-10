41854b2e14eeb49417fc0cf58ecd176c344b6333
Zachary Mark <zachary-mark@github.com>
2024-01-10 09:57:21 -0600

    repo-blob:  Compute absolute paths from non-absolute specs, support tree view

--- bin/repo-blob.sh
+++ bin/repo-blob.sh
@@ -10,7 +10,20 @@ fi
 
 BASE_URL="https://github.com"
 repo_nwo=$(git config --get remote.origin.url | tr ':' ' ' | awk '{print $NF}')
+repo_base_dir="$(git rev-parse --show-toplevel)/"
 blob_branch=${BLOB_BRANCH:-$(git status | head -1 | awk '{print $NF}')}
 file_path="$1"
+absolute_file_path="$(pwd)/$file_path"
 
-echo "$BASE_URL/$repo_nwo/blob/$blob_branch/$file_path"
+if [ ! -f "$absolute_file_path" ] ; then
+  echo "error: $absolute_file_path does not exist"
+  exit 1
+fi
+
+net_file_path=${absolute_file_path#$repo_base_dir}
+browse_type="blob"
+if [ -d "$absolute_file_path" ] ; then
+  browse_type="tree"
+fi
+
+echo "$BASE_URL/$repo_nwo/$browse_type/$blob_branch/$net_file_path"
